ef58a7ce773d7981baceb3956dbcd47bfbb82daa
19alfonso00 <90704782+19alfonso00@users.noreply.github.com>
2021-11-19 12:47:59 -0600

    Add files via upload

---
+++ PRACTICA 09/practica9.ps1
@@ -0,0 +1,5 @@
+﻿
+$DNS =  ipconfig /displaydns 
+$ruta = "E:\DE PROMAX\5to semestre\LPC\PRACTICA 09\result_practica9.txt"
+$cod = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($DNS))
+Set-Content -Value "$cod" -Path $ruta
